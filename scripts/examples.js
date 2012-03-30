a514d84a49799f3c80b4d39810e72f1e36268987
mrchief <mrchief_2000@yahoo.com>
2012-03-29 23:49:17 -0400

    Adding more docs

---
+++ scripts/examples.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+	$('#nav').on('click', 'ul a', function () {
+		var uid = $(this).data('uid');
+		if (uid) {
+			$('#examples iframe').attr('src', 'http://razorpad.apphb.com/' + uid);
+			return false;
+		}
+	});
+} (jQuery));
